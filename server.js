897f915951f4903ffcb2d000d6a9c3ca7b97a57e
Dnam00 <hyunda@live.unc.edu>
2022-12-05 01:25:57 -0500

    changed files.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 import minimist from "minimist";
 import express from "express";
-import {roll} from "../lib/roll.js";
+import {roll} from "./lib/roll.js";
 
 
 
@@ -13,43 +13,43 @@ const port = args.port || 5000;
 
 //http header
 app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+//app.use(express.json());
 
-app.get("/app/", function(req, res) => {
+app.get("/app/", (req, res) => {
 	res.status(200);	
 	res.send("200 OK");	
 });
 // case is made below
-//app.get("/app/roll/", function(req, res) => {	
-//	res.send(roll(6,2,1));
-//});
+app.get("/app/roll/", (req, res) => {	
+	res.send(roll(6,2,1));
+});
 
 // base case for 6, 2, 1 and taking the data body for sides, dice, and rolls
-app.get("/app/roll/", function(req, res) => {
-	let sides = parseint( req.body.sides) || 6;
-	let dice =  parseint( req.body.dice) || 2;
-	let rolls = parseint( req.body.rolls) || 1;
+app.get("/app/roll/", (req, res, next) => {
+	let sides = parseInt(req.body.sides) || 6;
+	let dice =  parseInt(req.body.dice) || 2;
+	let rolls = parseInt(req.body.rolls) || 1;
 	res.send(roll(sides, dice, rolls)).end();
 });
 //  returns JSON whatever sides is specified in parameter
-app.get("/app/roll/:sides/", function(req, res) => {
-	let sides = parseint(req.params.sides);
+app.get("/app/roll/:sides/", (req, res, next) => {
+	let sides = parseInt(req.params.sides);
 	let dice = 2;
 	let rolls = 1;
 	res.send(roll(sides,dice,rolls)).end();
 });
 //  returns JSON whatever sides and number of dices is specified in parameter
-app.get("/app/roll/:sides/:dice/", function(req, res) => {
-	let sides = parseint(req.param.sides);
-	let dice = parseint(req.param.dice);
+app.get("/app/roll/:sides/:dice/", (req, res, next) => {
+	let sides = parseInt(req.params.sides);
+	let dice = parseInt(req.params.dice);
 	let rolls = 1;
 	res.send(roll(sides,dice,rolls)).end();
 });
 //  returns JSON whatever sides, number of dices, and number of rolls specified in parameter
-app.get("/app/roll/:sides/:dice/:rolls/", function(req, res) => {
-	let sides = parseint(req.param.sides);
-	let dice = parseint(req.param.dice);
-	let rolls = parseint(req.param.rolls);
+app.get("/app/roll/:sides/:dice/:rolls/", (req, res, next) => {
+	let sides = parseInt(req.params.sides);
+	let dice = parseInt(req.params.dice);
+	let rolls = parseInt(req.params.rolls);
 	res.send(roll(sides,dice,rolls)).end();
 });
 
